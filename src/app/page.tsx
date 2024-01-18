583eec045c0176e0165ae17c695cace888b92ac7
Aryo <chiefdevica@gmail.com>
2024-01-18 21:16:16 +0700

    feat: add eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
 import { ModeToggle } from './toggle'
+import * as React from 'react'
 
-export default function Home() {
+export default function Home (): JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ModeToggle></ModeToggle>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <ModeToggle></ModeToggle>
+      </main>
+    </>
   )
 }
